421bc0022c855d4224f80fe31decf128088b657c
Abkreno <mohamedelhamamsy789@gmail.com>
2015-02-27 21:22:03 +0200

    Adds Premit_Params to user,post comment To visit admin site go to http://localhost:3000/admin User : admin@example.com Password : password Becarful when adding authentication to users don't leave the encrypted password field empty (copy and paste it in the field)

--- app/admin/comment.rb
+++ app/admin/comment.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Comment do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  # permit_params :list, :of, :attributes, :on, :model
+   permit_params :body, :created_at, :updated_at, :post_id, :user_id
   #
   # or
   #
--- app/admin/user.rb
+++ app/admin/user.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register User do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  # permit_params :list, :of, :attributes, :on, :model
+   permit_params :created_at, :updated_at, :email, :encrypted_password, :reset_password_token , :reset_password_sent_at , :remember_created_at , :sign_in_count , :current_sign_in_at , :last_sign_in_at , :current_sign_in_ip , :last_sign_in_ip , :profile_picture_file_name , :profile_picture_content_type , :profile_picture_file_size , :profile_picture_updated_at , :authorized
   #
   # or
   #
