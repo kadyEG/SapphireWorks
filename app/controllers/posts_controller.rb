046c4d5282ec0fe27de61bf07850ef7715495656
Sherif Mowafy <sherif.mowafy@gmail.com>
2015-02-27 18:43:36 +0200

    conflicts in posts controller file resolved

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,46 +1,10 @@
+
 class PostsController < ApplicationController
 
   def index
-
-
-    @posts = Post.all
-  end
- def show
-    @post = Post.find(params[:id])
-  end
-
-  def new
-  end
-def create
-  # @post = Post.new(params[:post])
-
-  # @post.save
-  # redirect_to @post
-   @post = Post.new(post_params)
-
-    if @post.save
-      redirect_to @post
-    else
-      render 'new'
-    end
-end
-def destroy
-  @post = Post.find(params[:id])
-  @post.destroy
- 
-  redirect_to posts_path
-end
-private
-  def post_params
-    params.require(:post).permit(:user_id, :title, :body)
-  end
-
-		@posts = Post.all
-	end
-
     @posts = Post.search(params[:search])
     @categ = Post.select(:title).distinct
-
+  end
 
 
 	def show
@@ -55,14 +19,21 @@ private
 	def create
 		@post = Post.new(post_params)
 
-    @post.user_id = current_user.id
+    		@post.user_id = current_user.id
 
 		if @post.save
 			redirect_to @post
 		else
-      flash[:alert] = "Make sure that you filled in the title and body"
+			flash[:alert] = "Make sure that you filled in the title and body"
 			render 'new'
 		end
+	end
+
+	def destroy
+	  @post = Post.find(params[:id])
+	  @post.destroy
+	 
+	  redirect_to posts_path
 	end
 
 
@@ -72,4 +43,5 @@ private
 		params.require(:post).permit(:title, :body, :videourl)
 	end
 
+end
 
